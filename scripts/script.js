26eb85e8abab03cd0ab091ffc6d8b3f53db75198
p <prateek6587@gmail.com>
2018-04-19 14:57:58 +0530

    My first commit

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+//alert("Is the file attached");
+
+function calc() {
+    var w = document.getElementById("width").value;
+    var h = document.getElementById("height").value;
+    var area = w * h;
+    document.getElementById("answer").value = area;
+}
